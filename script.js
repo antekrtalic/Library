d42c09a6130c2ec45dbd206f278f428228cfa0d4
Ante Dragoje <antekrtalic2@gmail.com>
2021-08-13 18:26:23 +0200

    Added function to display all books from library

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let myLibrary = [];
+const myLibrary = [];
+const author = document.getElementById('author').value;
+const title = document.getElementById('title').value;
+const pages = document.getElementById('read').value;
+const read = Document.getElementById('read');
 
 function Book(author, title, pages, read, year) {
     this.author = author;
@@ -7,7 +11,21 @@ function Book(author, title, pages, read, year) {
     this.read = read;
     this.year = year;
 }
+// Add book to library after submission
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+    if (book.author == '' || book.title == '' || book.pages == '' || book.read == '' || book.year == '') {
+        throw("Error, fulfill all fields!");
+    } else {
+        myLibrary.push(book);
+    }
+    
+}
+
+// Display all books from myLibrary
+
+function listOfBooks(library) {
+    for (let book in library) {
+        console.log(book);
+    }
 }
